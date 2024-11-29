31ddfbead68868aea357a5da48bbabc6eaa4c6d0
TheAnh6501 <tta6501.work@gmail.com>
2024-11-29 14:50:04 +0700

    new update

--- app.py
+++ app.py
@@ -9,8 +9,20 @@ todoList = [
 ] 
 
 @app.route('/')
-def index():
+def index(): 
     return render_template('index.html', todoList=todoList)
 
+@app.route('/add', methods=['POST'])
+def add():
+    description = request.form['description']
+    todoList.append({'id' : len(todoList) + 1, 'description' : description, 'status' : 'Done'})
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    for todo in todoList:
+        if todo['id'] == id:
+            todoList.remove(todo)
+    return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run(debug=True)
